eab1893b5296fa4a406c735e31589692b865618d
Anastasiia211 <nastik121@ukr.net>
2023-08-20 15:49:06 +0300

    8 hm

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
+    const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+    
+player.on('timeupdate', throttle(onTime, 1000));
 
+function onTime(data) {
+    let time = data.seconds;
+    localStorage.setItem('videoplayer-current-time',time)
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const savedTime = localStorage.getItem('videoplayer-current-time');
+    if (savedTime !== null) {
+        player.setCurrentTime(parseFloat(savedTime))
+            .then(function (seconds) {
+            
+            })
+
+            .catch(function (error) {
+                switch (error.name) {
+                    case 'RangeError':
+            
+                        break;
+
+                    default:
+          
+                        break;
+                }
+            });
+    }
+ });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector(".feedback-form");
 
+let dataForm = {};
+
+form.addEventListener("input", throttle (event => {
+    event.preventDefault();
+    dataForm[event.target.name] = event.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(dataForm));
+}, 500))
+
+window.addEventListener("load", () => {
+    const savedForm = JSON.parse(localStorage.getItem("feedback-form-state"));
+    if (savedForm) {
+        dataForm = savedForm;
+        populateDataForm(dataForm);
+    }
+}),
+    
+    form.addEventListener("submit", evt => {
+        evt.preventDefault();
+        console.log(dataForm)
+        localStorage.removeItem("feedback-form-state");
+        dataForm = {};
+        form.reset ();
+    });
+
+function populateDataForm(data) {
+    for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+            form.elements[key].value = data[key];
+            }
+        }
+    }
